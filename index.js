ea833f76b9a2f7d83d19f7ade56d9b61ee184019
Tamim Mahmud <internationalinf24@gmail.com>
2022-07-10 00:45:14 +0600

    Variable and unction type process in ES6

---
+++ index.js
@@ -0,0 +1,65 @@
+//function type before es6
+function multiply1(num1, num2){
+    result = num1 + num2;
+    return result;
+}
+var input = multiply1(5, 6);
+console.log(input);
+
+
+//type single function after es6
+const multiply2 = num => num * 2;
+
+var result2 = multiply2( 13);
+console.log(result2);
+
+
+//type single function after es6
+const multiply3 = (num1, num2) => (num1 + num2);
+
+var result3 = multiply3( 13, 12);
+console.log(result3);
+
+function myName(nam5, nam6){
+    let namResult = nam5 + nam6;
+    return namResult;
+}
+let newNames = myName(15, 100);
+console.log(newNames);
+
+
+//function without 2nd value no:1
+function myName1(nam5, nam6){
+    if (nam6 == undefined);
+    nam6 = 0;
+
+    let namResult = nam5 + nam6;
+    return namResult;
+}
+let newNames1 = myName1(15);
+console.log(newNames1);
+
+
+//function without 2nd value no:1
+function myName2(nam7, nam8){
+    nam8 = nam8|| 0;
+
+    let namResult1 = nam7 + nam8;
+    return namResult1;
+}
+let newNames2 = myName2(155);
+console.log(newNames2);
+
+//array and number value's output process simple
+
+let firstClass = "Sakib Ali khan";
+let lastClass = "Salman Ali khan";
+let totalClass = firstClass + " and "+ lastClass + " they are two class captains are vary good."
+console.log(totalClass);
+
+//now the different way
+
+let firstLaptop = "Toshiba";
+let lastLaptop = "HP";
+let totalLaptop = `${firstLaptop} and ${lastLaptop} are very good laptop.`;
+console.log(totalLaptop);
